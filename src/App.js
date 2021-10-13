b324fcf89b4210c47211bc598f94c49620f9010d
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-10-13 21:50:47 +0530

    Fetch using axios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import fetchMovieData from "./api/apiDetails";
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [showName, setShowName] = useState("");
+
+  useEffect(() => {
+    fetchMovieData()
+      .then((data => {
+        console.log(data[0])
+        setShowName(JSON.stringify(data))
+      }))
+      .catch((err => console.log(err)))
+
+    // setShowName(data);  
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Details</h1>
+
+      <p>{showName[1]}</p>
     </div>
   );
 }
 
 export default App;
+
+
+// confirmed.value
---
+++ src/api/apiDetails.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+
+
+const fetchMovieData = async () => {
+    try {
+        const {data} = await axios.get("https://api.tvmaze.com/search/shows?q=all");
+        
+        // const i = 0;
+        
+        const modifiedData = [
+            data[0].show,
+            // data[1].show,
+            // data[2].show,
+            // data[3].show,
+            // data[4].show,
+            // data[5].show,
+            // data[6].show,
+            // data[7].show,
+            // data[8].show,
+            // data[9].show,
+        ]
+                
+        
+        
+        
+        console.log(modifiedData)
+
+        return modifiedData;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default fetchMovieData;

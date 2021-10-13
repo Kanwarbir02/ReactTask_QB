8321c886265b2d0d1d06e925da98a644a89f73fd
Kanwarbir02 <kanwarbir2002@gmail.com>
2021-10-14 02:16:44 +0530

    placing all listed components(unstyled)

--- src/components/frontpage.js
+++ src/components/frontpage.js
@@ -17,10 +17,18 @@ const Frontpage = () => {
     }, []);
 
     const dataList = showData.map((data) =>
+    <div key={data.show.id} margin="30px">
+        <li >
+        {data.show.name}
+        </li>
 
-    <li key={data.show.id}>
-      {data.show.name}
-    </li>
+        <img src={data.show.image.original} alt="No Image Found" height="40px" width="60px"/>
+
+        <p>{data.show.summary}</p>
+
+        <button>Click Here to book your Ticket!</button>
+    </div>
+        
 
     
   );
